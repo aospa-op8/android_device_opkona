1b19086f753d4e5bd8ecca0d7df565cf3c2b3106
Marco Marinho <marcomarinho12@gmail.com>
2023-04-20 21:36:06 +0100

    add audio configs to op8(pro)

--- device.mk
+++ device.mk
@@ -45,7 +45,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info_intcodec.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info_intcodec.xml \
     $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_mixer_paths.xml \
-    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_platform_info.xml
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_ODM)/etc/sound_trigger_platform_info.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml-instantnoodle:$(TARGET_COPY_OUT_VENDOR)/19821/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml-instantnoodlep:$(TARGET_COPY_OUT_VENDOR)/19811/etc/mixer_paths.xml
 
 PRODUCT_ODM_PROPERTIES += \
     ro.config.vc_call_vol_steps=9 \
